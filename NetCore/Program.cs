a2b257daacf4b0b68556a56beab1bab79549b25d
Paula Castro <paulacastro100@gmail.com>
2018-04-03 23:55:11 -0300

    add exemplos dotnet core

---
+++ NetCore/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World da Paula!");
+            //https://mva.microsoft.com/en-us/training-courses/asp-net-core-beginner-18153?l=VM5gy36dE_6611787171
+        }
+    }
+}
