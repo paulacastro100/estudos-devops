b31987fddf505f2c984f181687abc04546c9679e
Paula Castro <paulacastro100@gmail.com>
2018-04-06 00:52:24 -0300

    add arquivos NETCORE

--- NetCore/Program.cs
+++ NetCore/Program.cs
@@ -8,6 +8,12 @@ namespace NetCore
         {
             Console.WriteLine("Hello World da Paula!");
             //https://mva.microsoft.com/en-us/training-courses/asp-net-core-beginner-18153?l=VM5gy36dE_6611787171
+            Console.WriteLine("\nWhat is your name?");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true);
         }
     }
 }
